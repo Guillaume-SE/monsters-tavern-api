b5d5bec592f6319afb084878addf03c0161bde9e
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-01-25 12:50:25 +0100

    add unfollow routes

--- src/controllers/follow.js
+++ src/controllers/follow.js
@@ -3,22 +3,51 @@ import Monster from "../models/monster.js";
 export const follow = async (req, res, next) => {
     try {
         const connectedMonster = req.monster;
-        console.log("connectedMonster", connectedMonster);
         const monsterToFollow = await Monster.findById(req.params.id);
-        console.log("monsterToFollow", monsterToFollow);
 
+        if (connectedMonster._id.equals(monsterToFollow._id)) {
+            const message = "Vous ne pouvez pas suivre votre propre compte.";
+            return res.status(403).json({ message });
+        }
+        if (monsterToFollow.followers.includes(connectedMonster._id)) {
+            const message = "Vous suivez déjà cette personne";
+            return res.status(403).json({ message });
+        }
         if (!monsterToFollow.followers.includes(connectedMonster._id)) {
             monsterToFollow.followers.push(connectedMonster._id);
             await monsterToFollow.save();
 
             connectedMonster.following.push(monsterToFollow._id);
             await connectedMonster.save();
+
+            const message = `Vous suivez désormais ${monsterToFollow.name}.`;
+            res.status(200).json({ message });
         }
-        const message = `Vous suivez désormais ${monsterToFollow.name}.`;
-        res.status(200).json({ message });
 
     } catch (error) {
         const message = "L\'ajout de ce monstre à vos abonnements à échoué. Veuillez réessayer.";
+        res.status(500).json({ message });
+    }
+};
+
+export const unfollow = async (req, res, next) => {
+    try {
+        const connectedMonster = req.monster;
+        const monsterToUnfollow = await Monster.findById(req.params.id);
+
+        if (connectedMonster.following.includes(monsterToUnfollow._id)) {
+            connectedMonster.following.pull(monsterToUnfollow._id);
+            await connectedMonster.save();
+
+            monsterToUnfollow.followers.pull(connectedMonster._id);
+            await monsterToUnfollow.save();
+
+            const message = `Vous ne suivez plus ${monsterToUnfollow.name}.`;
+            res.status(200).json({ message });
+        }
+
+    } catch (error) {
+        const message = "La supression du monstre de votre liste à échoué. Veuillez réessayer.";
         res.status(500).json({ message });
     }
 };
--- src/routes/follow.js
+++ src/routes/follow.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import authentication from '../middlewares/authentication.js';
-import { follow } from '../controllers/follow.js';
+import {
+    follow,
+    unfollow
+} from '../controllers/follow.js';
 
 const router = express.Router();
 
 router.patch('/follow/:id', authentication, follow);
+
+router.patch('/unfollow/:id', authentication, unfollow);
 
 export default router;
